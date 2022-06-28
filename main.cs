badbdcb64040c792effb4b7091e0426fa4c93dc7
YoumnaMohamed <63843981+YoumnaMohamed@users.noreply.github.com>
2022-06-28 18:11:37 +0200

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,15 @@
+using System
+
+namespace HelloYoumna
+{
+ class Program
+       {
+           static void Main(string[] args) {
+char name = "Youmna"
+int age = 22
+
+Console.WriteLine("Hi I am"+ name);
+Console.WriteLine("My age is"+ age);
+             }
+        }
+}
